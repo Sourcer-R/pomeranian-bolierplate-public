d3cb9ef114f0c615956439a283674caa8d7dde4b
Sourcer-R <mariam8yunis@gmail.com>
2023-07-06 15:23:06 +0200

    New features to styling and JS added

--- src/App/Exercises/Js/HitTheMoleGame/HitTheMoleGame.jsx
+++ src/App/Exercises/Js/HitTheMoleGame/HitTheMoleGame.jsx
@@ -3,11 +3,7 @@ import React from 'react';
 import Mole from './mole.svg';
 import { useState } from 'react';
 
-export function HitTheMoleGame() {
-  const [moleArray, setMoleArray] = useState(
-    Array(10).fill({ isVisible: false, isWhacked: false })
-  );
-
+export const MoleGameSettings = () => {
   const defaultGameTime = 2 * 60 * 1000;
   const [gameTime, setGameTime] = useState(defaultGameTime); //2000 * 60 Zeit
   const [moleCount, setMoleCount] = useState(1);
@@ -24,40 +20,77 @@ export function HitTheMoleGame() {
     { label: '3 moles' },
   ];
 
+  return (
+    <>
+
+<div className="moleGameOptions">
+      <p>
+      A game of following the mole and hitting the square where it appeared.
+      </p>
+      <div className="gameOptionsButtons">
+        <div className="gameButtonsRows">
+          <div>
+            <h4>GAME TIME</h4>
+
+            {gameTimeOption.map(({ label, timeValue }) => (
+              <button
+                className={gameTime === timeValue ? 'activeButton' : ''}
+                onClick={() => setGameTime(timeValue)}
+              >
+                {label}
+              </button>
+            ))}
+          </div>
+
+          <div>
+            <h4>AMOUNT OF MOLES</h4>
+
+            {moleCountOption.map(({ label }) => (
+              <button
+                className={moleCount === Number(label[0]) ? 'activeButton' : ''}
+                onClick={() => setMoleCount(Number(label[0]))}
+              >
+                {label}
+              </button>
+            ))}
+          </div>
+
+          <div>
+            <h4>CONTROLS</h4>
+
+            <button>START</button>
+          </div>
+        </div>
+      </div>
+      </div>
+    </>
+  );
+};
+
+export const MoleGameBoard = ({ moleArray, hitTheMole }) => {
+  return moleArray.map((mole, index) => (
+    <span>
+      {mole.isVisible && (
+        <img onClick={() => hitTheMole(index)} src={Mole} alt="" />
+      )}
+    </span>
+  ));
+};
+
+export function HitTheMoleGame() {
+  const [moleArray, setMoleArray] = useState(
+    Array(10).fill({ isVisible: false, isWhacked: false })
+  );
+
   function hitTheMole(index) {
     if (!moleArray[index].isWhacked) return;
     moleArray[index].isWhacked = !moleArray[index].isWhacked;
   }
 
   return (
-    <div>
-
-      <p className='paragraf-header' >A game of following the mole and hitting the square where it appeared. </p>
-      <p> AMOUNT OF MOLES {moleCount}</p>
-
-      {moleCountOption.map(({ label }) => (
-        <button onClick={() => setMoleCount(Number(label[0]))}>{label}</button>
-      ))}
-
-      <p className='game-time'>
-        GAME TIME {gameTime / 60 / 1000}
-        {gameTime > 1 * 60 * 1000 ? ' minutes' : ' minute'}
-      </p>
-      {gameTimeOption.map(({ label, timeValue }) => (
-        <button onClick={() => setGameTime(timeValue)}> {label} </button>
-      ))}
-
-      {moleArray.map((mole, index) => (
-        <span>
-          {mole.isVisible && (
-            <img onClick={() => hitTheMole(index)} src={Mole} alt="" />
-          )}
-        </span>
-      ))}
-
-      <p> CONTROLS</p>
-<div className='controls-button'>
-      <button>START</button></div>
-    </div>
+    <>
+      <MoleGameSettings />
+      <MoleGameBoard moleArray={moleArray} hitTheMole={hitTheMole} />
+    </>
   );
 }
--- src/App/Layout/AppAside.jsx
+++ src/App/Layout/AppAside.jsx
@@ -3,6 +3,14 @@ import { NavLink } from 'react-router-dom';
 import './styles/aside.css';
 
 import { FaqIcon } from '../Components/Icons/FaqIcon';
+import { HouseIcon } from '../Components/Icons/HouseIcon';
+import { PersonalCardIcon } from '../Components/Icons/PersonalCardIcon';
+import { EditIcon } from '../Components/Icons/EditIcon';
+import { CalendarIcon } from '../Components/Icons/CalendarIcon';
+import { ElementIcon } from '../Components/Icons/ElementIcon';
+import { BookIcon } from '../Components/Icons/BookIcon';
+
+
 
 
 export function AppAside() {
@@ -11,24 +19,46 @@ export function AppAside() {
     <aside>
       <nav>
         <ul>
-          <li>
-            <NavLink to="dashboard">Dashboard</NavLink>
-          </li>
-          <li>
-            <NavLink to="blocks">Bloki</NavLink>
-          </li>
-          <li>
-            <NavLink to="exercises">Ćwiczenia</NavLink>
-          </li>
-          <li>
-            <NavLink to="blog">Blog</NavLink>
-          </li>
-          <li>
+          
+            <NavLink className= "aside-row" to="dashboard">
+              <HouseIcon className='menu-icon'/>
+              
+              Dashboard</NavLink>
+          
+
+              <NavLink className="aside-row"to="cv">
+              <PersonalCardIcon className='menu-icon'/>
+                Moje CV</NavLink>
+         
+            <NavLink className="aside-row" to="blocks">
+             
+               <ElementIcon className='menu-icon'/>
+              Bloki</NavLink>
+         
+          
+            <NavLink className="aside-row" to="exercises">
+            <EditIcon className='menu-icon'/>
+              
+              Ćwiczenia</NavLink>
+         
+          
+              <NavLink className="aside-row" to="kalendarz">
+                <CalendarIcon className='menu-icon'/>
+                
+                Kalendarz</NavLink>
+
+            <NavLink className="aside-row" to="blog">
+
+              <BookIcon className='menu-icon' />
+             
+              Blog</NavLink>
+         
+          
             <NavLink className="aside-row" to="faq">
               <FaqIcon className="menu-icon" />
               FAQ
             </NavLink>
-          </li>
+         
         </ul>
       </nav>
       <p style={{ padding: '1rem 0' }}>Sidebar items, widgets, etc</p>
